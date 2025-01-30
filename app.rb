3aa86f605db2c046199412b234d2cd459553ebe8
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-01-30 05:58:02 +0000

    Draft 1

--- app.rb
+++ app.rb
@@ -1,9 +1,42 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home_page)
+end
+
+get("/rock") do
+  @we_choice = "rock"
+
+  choices = ["rock","paper","scissors"]
+
+  @they_choice = choices.sample
+
+  #Rule logic
+  
+    if @they_choice == "rock"
+      @outcome = "We tied!"
+    elsif @they_choice == "paper"
+      @outcome = "We lost!"
+    else
+      @outcome = "We won!"
+    
+    end
+
+  erb(:rock)
+
+end
+
+get("/paper") do
+
+end
+
+get("/scissors") do
+
 end
